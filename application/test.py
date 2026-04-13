9a83aef0a51afb179c7a1c5c0bb22f56ebd9c36a
Bradley <bradleysemail2@gmail.com>
2026-04-12 20:26:05 -0400

    improved tests

--- application/test.py
+++ application/test.py
@@ -1,9 +1,45 @@
-import emailgetter
+import sys
 
-emailgetter.init()
+blue = "\033[34m"
+reset = "\033[0m"
 
-otp = emailgetter.getLatestOTP()
-if otp:
-    print(otp)
+if len(sys.argv) > 1:
+    test = sys.argv[1]
 else:
-    print("a stinky error happened")
+    print("Please specify a test to run:" + blue + "\npython test.py <test name>" + reset)
+    sys.exit()
+print(f'Attempting to run test: "{test}"')
+
+if test == "otp":
+    print("Running test")
+    import emailgetter
+
+    emailgetter.init()
+
+    otp = emailgetter.getLatestOTP()
+    if otp:
+        print(otp)
+    else:
+        print("a stinky error happened")
+
+elif test == "link":
+    print("Running test")
+    import emailgetter
+
+    emailgetter.init()
+
+    link = emailgetter.getLatestLink()
+    if link:
+        print(link)
+    else:
+        print("a stinky error happened")
+
+elif test == "paste":
+    print("Running test")
+    from paste import pasteText
+    import time
+    time.sleep(3)
+    pasteText("yay it works :D")
+
+else:
+    print("No such test found")
