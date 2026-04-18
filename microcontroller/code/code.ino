f0aaaa6748ea7f579c9e34bbcc2fa5e0078527c3
Bradley <bradleysemail2@gmail.com>
2026-04-18 19:12:18 -0400

    added libraries

--- microcontroller/code/code.ino
+++ microcontroller/code/code.ino
@@ -1,12 +1,21 @@
 #include Serial.h
+#include String.h
 
 const int SDA_PIN = D5;
 const int SCL_PIN = D6;
 
+const String possibleMessages = ["pasted-otp", "pasted-link", "otp-paste-failed", "link-paste-failed"]
+String serialBuffer = ""
 int currentDisplay = 0; // 0=home screen, 1=loading, 2=success, 3=failure, 4=disconnect
 
 void updateDisplay() {
 
+}
+
+void waitForMessage() {
+  while(true) {
+    
+  }
 }
 
 void otpPressed() {
