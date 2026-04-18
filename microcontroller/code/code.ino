57b279b1a8c1bf6a4f4e048e745f872f2d09a1ab
Bradley <bradleysemail2@gmail.com>
2026-04-18 19:01:15 -0400

    added some code

--- microcontroller/code/code.ino
+++ microcontroller/code/code.ino
@@ -1,5 +1,23 @@
+#include Serial.h
+
 const int SDA_PIN = D5;
 const int SCL_PIN = D6;
+
+int currentDisplay = 0; // 0=home screen, 1=loading, 2=success, 3=failure, 4=disconnect
+
+void updateDisplay() {
+
+}
+
+void otpPressed() {
+  Serial.print("paste-otp");
+  currentDisplay = 1
+}
+
+void linkPressed() {
+  Serial.print("paste-link");
+  currentDisplay = 1
+}
 
 void setup() {
   pinMode(SDA_PIN, OUTPUT);
@@ -7,6 +25,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
