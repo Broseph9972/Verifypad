a9645cadf9c1683d4a2c90653e0d0eaa71b35b95
Bradley <bradleysemail2@gmail.com>
2026-04-18 11:36:43 -0400

    stuff

---
+++ microcontroller/code/code.ino
@@ -0,0 +1,12 @@
+const int I2CD = D6;
+const int I2CC = D7;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
