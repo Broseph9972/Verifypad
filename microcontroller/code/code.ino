3ec805a199e5a92189f5d52c5d3f5f2bc869dc41
Bradley <bradleysemail2@gmail.com>
2026-04-18 20:59:18 -0400

    got rid of joes ai slop

--- microcontroller/code/code.ino
+++ microcontroller/code/code.ino
@@ -8,36 +8,12 @@ static UBYTE displayBuffer[OLED_0in49_WIDTH * OLED_0in49_HEIGHT / 8];
 static char serialBuffer[24];
 static uint8_t serialBufferLength = 0;
 
-<<<<<<< HEAD
-int currentDisplay = 0; // 0=home screen, 1=loading, 2=success, 3=failure, 4=disconnect
-
-void displayUpdater(const char *message) {
-  Paint_SelectImage(displayBuffer);
-  Paint_Clear(WHITE);
-
-  int messageWidth = strlen(message) * Font8.Width;
-  int x = 0;
-  if (messageWidth < OLED_0in49_WIDTH) {
-    x = (OLED_0in49_WIDTH - messageWidth) / 2;
-  }
-
-  int y = (OLED_0in49_HEIGHT - Font8.Height) / 2;
-  Paint_DrawString_EN(x, y, message, &Font8, BLACK, WHITE);
-  OLED_0in49_Display_RAM(displayBuffer);
-}
-
-void displayUpdater(int displayToShow) {
-  currentDisplay = displayToShow;
-
-  switch (displayToShow) {
-=======
 const String possibleMessages[] = {"pasted-otp", "pasted-link", "otp-paste-failed", "link-paste-failed"};
 const int possibleMessagesCount = sizeof(possibleMessages) / sizeof(possibleMessages[0]);
 int currentDisplay = 0; // 0=home screen, 1=loading, 2=success, 3=failure, 4=disconnect
 
 void displayUpdater() {
   switch (currentDisplay) {
->>>>>>> abf18d6dbf4518a18c496838f8caef0313ff68b2
     case 0:
       displayUpdater("HOME");
       break;
@@ -60,18 +36,6 @@ void displayUpdater() {
   }
 }
 
-<<<<<<< HEAD
-bool processIncomingMessage(const char *message) {
-  if (strcmp(message, "pasted-otp") == 0 || strcmp(message, "pasted-link") == 0) {
-    displayUpdater(2);
-    return true;
-  } else if (strcmp(message, "otp-paste-failed") == 0 || strcmp(message, "link-paste-failed") == 0) {
-    displayUpdater(3);
-    return true;
-  } else if (strcmp(message, "disconnect") == 0) {
-    displayUpdater(4);
-    return true;
-=======
 void waitForMessage() {
   const unsigned long responseTimeoutMs = 25000;
   unsigned long startTime = millis();
@@ -100,7 +64,6 @@ void waitForMessage() {
     }
 
     delay(10);
->>>>>>> abf18d6dbf4518a18c496838f8caef0313ff68b2
   }
 
   return false;
